9ce8a23083c9c20054f0445e2377973a81be894a
crisahu777 <cris.ahu777@gmail.com>
2022-07-28 19:05:11 -0300

    ultima subida

---
+++ js/java.js
@@ -0,0 +1,13 @@
+function redirigir(){
+    var respuesta = confirm("Estas por salir de nuesta pagina, desea continuar?")
+    return respuesta;
+}
+
+function borrar(){
+    var respuesta = Window.confirm("Estas seguro que quieres borrar todo los datos?")
+    return respuesta;
+}
+
+function confirmacion (){
+    alert("Consulta enviada con exito.")
+}
